a34897f9c8311ca9fbc89a4c1245af8fefac4cc2
= <=>
2021-02-05 21:34:48 +0700

    Refactoring Class

--- app/src/main/java/project/achsan/quotesapps/ui/global_quotes/GlobalQuotesModel.kt
+++ app/src/main/java/project/achsan/quotesapps/ui/global_quotes/GlobalQuotesModel.kt
@@ -1,13 +1,13 @@
-package project.achsan.quotesapps.ui.home
+package project.achsan.quotesapps.ui.global_quotes
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class GlobalQuotesModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is notifications Fragment"
     }
     val text: LiveData<String> = _text
 }
--- app/src/main/java/project/achsan/quotesapps/ui/my_quotes/MyQuotesFragment.kt
+++ app/src/main/java/project/achsan/quotesapps/ui/my_quotes/MyQuotesFragment.kt
@@ -1,4 +1,4 @@
-package project.achsan.quotesapps.ui.notifications
+package project.achsan.quotesapps.ui.my_quotes
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,20 +10,20 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import project.achsan.quotesapps.R
 
-class NotificationsFragment : Fragment() {
+class MyQuotesFragment : Fragment() {
 
-    private lateinit var notificationsViewModel: NotificationsViewModel
+    private lateinit var myQuotesModel: MyQuotesModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        notificationsViewModel =
-                ViewModelProvider(this).get(NotificationsViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
-        val textView: TextView = root.findViewById(R.id.text_notifications)
-        notificationsViewModel.text.observe(viewLifecycleOwner, Observer {
+        myQuotesModel =
+                ViewModelProvider(this).get(MyQuotesModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_my_quotes, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        myQuotesModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root

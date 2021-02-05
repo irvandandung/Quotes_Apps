87eedccef3185d478d2792d8f442d9cf7fba93e7
= <=>
2021-02-05 22:53:13 +0700

    add rule session in mainactivity

--- app/src/main/java/project/achsan/quotesapps/MainActivity.kt
+++ app/src/main/java/project/achsan/quotesapps/MainActivity.kt
@@ -1,15 +1,21 @@
 package project.achsan.quotesapps
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import project.achsan.quotesapps.models.Token
+import project.achsan.quotesapps.ui.LoginActivity
+import project.achsan.quotesapps.utils.TokenPref
 
 class MainActivity : AppCompatActivity() {
-
+    private lateinit var tokenPref: TokenPref
+    private lateinit var token: Token
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,5 +28,12 @@ class MainActivity : AppCompatActivity() {
                 R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+        tokenPref = TokenPref(this)
+        token = tokenPref.getToken()
+        if (TextUtils.isEmpty(token.token)) {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }

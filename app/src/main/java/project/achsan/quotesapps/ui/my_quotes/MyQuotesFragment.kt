211e0ec91aa3c3501ccaf000f5cc8e6eff07de47
= <=>
2021-02-06 09:38:43 +0700

    add my quotes fragment

--- app/src/main/java/project/achsan/quotesapps/ui/my_quotes/MyQuotesFragment.kt
+++ app/src/main/java/project/achsan/quotesapps/ui/my_quotes/MyQuotesFragment.kt
@@ -4,28 +4,71 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.fragment_my_quotes.*
+import org.jetbrains.anko.support.v4.onRefresh
 import project.achsan.quotesapps.R
+import project.achsan.quotesapps.adapter.QuoteAdapter
+import project.achsan.quotesapps.databinding.FragmentMyQuotesBinding
+import project.achsan.quotesapps.interfaces.MainView
+import project.achsan.quotesapps.models.Login
+import project.achsan.quotesapps.models.Quote
+import project.achsan.quotesapps.models.Token
+import project.achsan.quotesapps.presenters.MainPresenter
+import project.achsan.quotesapps.utils.CoroutineContextProvider
+import project.achsan.quotesapps.utils.TokenPref
 
-class MyQuotesFragment : Fragment() {
-
-    private lateinit var myQuotesModel: MyQuotesModel
-
+class MyQuotesFragment : Fragment(), MainView {
+    private lateinit var tokenPref : TokenPref
+    private lateinit var token: Token
+    private lateinit var adapter : QuoteAdapter
+    private lateinit var presenter:MainPresenter
+    private var quotes: MutableList<Quote> = mutableListOf()
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
-    ): View? {
-        myQuotesModel =
-                ViewModelProvider(this).get(MyQuotesModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_my_quotes, container, false)
-        val textView: TextView = root.findViewById(R.id.text_dashboard)
-        myQuotesModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState:
+        Bundle?
+    ): View? = inflater.inflate(R.layout.fragment_my_quotes, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val binding = FragmentMyQuotesBinding.bind(view)
+        binding.recyclerviewMyQuotes.layoutManager = LinearLayoutManager(activity)
+        tokenPref = TokenPref(requireActivity())
+        token = tokenPref.getToken()
+        adapter = QuoteAdapter(requireActivity())
+        binding.recyclerviewMyQuotes.adapter = adapter
+        presenter =
+            MainPresenter(this, CoroutineContextProvider())
+        progressbar.visibility = View.VISIBLE
+        presenter.getMyQuotes(token.token)
+        swiperefresh.onRefresh {
+            progressbar.visibility = View.INVISIBLE
+            presenter.getMyQuotes(token.token)
+        }
     }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.getMyQuotes(token.token)
+    }
+
+    override fun showMessage(messsage: String) {
+        Toast.makeText(requireActivity(),messsage, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun resultQuote(data: ArrayList<Quote>) {
+        quotes.clear()
+        adapter.listQuotes = data
+        quotes.addAll(data)
+        adapter.notifyDataSetChanged()
+        progressbar.visibility = View.INVISIBLE
+        swiperefresh.isRefreshing = false
+    }
+
+    override fun resultLogin(data: Login) {
+        TODO("Not yet implemented")
+    }
+
 }

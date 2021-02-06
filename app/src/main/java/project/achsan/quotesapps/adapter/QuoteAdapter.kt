7e4ecf1daf60a1a5e4875024f2ea641fce260374
= <=>
2021-02-06 15:20:05 +0700

    add feature insert, update, delete quotes

--- app/src/main/java/project/achsan/quotesapps/adapter/QuoteAdapter.kt
+++ app/src/main/java/project/achsan/quotesapps/adapter/QuoteAdapter.kt
@@ -2,6 +2,7 @@ package project.achsan.quotesapps.adapter
 
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +10,10 @@ import androidx.recyclerview.widget.RecyclerView
 import project.achsan.quotesapps.R
 import project.achsan.quotesapps.databinding.ItemQuoteBinding
 import project.achsan.quotesapps.models.Quote
+import project.achsan.quotesapps.ui.QuoteAddUpdateActivity
+import project.achsan.quotesapps.utils.helper.EXTRA_POSITION
+import project.achsan.quotesapps.utils.helper.EXTRA_QUOTE
+import project.achsan.quotesapps.utils.helper.REQUEST_UPDATE
 
 class QuoteAdapter(private val activity: Activity): RecyclerView.Adapter<QuoteAdapter.QuoteViewHolder>() {
     var listQuotes = ArrayList<Quote>()
@@ -32,6 +37,13 @@ class QuoteAdapter(private val activity: Activity): RecyclerView.Adapter<QuoteAd
             binding.tvItemTitle.text = quote.quote_name
             binding.tvItemDate.text = quote.created_at
             binding.tvItemDescription.text = quote.quote_description
+            binding.cvItemQuote.setOnClickListener{
+                val intent = Intent(activity,
+                    QuoteAddUpdateActivity::class.java)
+                intent.putExtra(EXTRA_POSITION, position)
+                intent.putExtra(EXTRA_QUOTE, quote)
+                activity.startActivityForResult(intent, REQUEST_UPDATE)
+            }
         }
     }
 }
